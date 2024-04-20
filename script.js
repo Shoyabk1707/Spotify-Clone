2e7f600657cca27d2c12be3fd31236d555f970c2
Shoyab Khan <156696944+Shoyabk1707@users.noreply.github.com>
2024-04-20 21:00:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/Spotify-clone/songs/");
+    let a = await fetch("https://shoyabk1707.github.io/Spotify-Clone/songs");
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div");
