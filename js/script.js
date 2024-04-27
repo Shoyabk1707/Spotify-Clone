278fa96f51e16724e97877031288d9458f70ded0
Shoyab Khan <156696944+Shoyabk1707@users.noreply.github.com>
2024-04-27 14:26:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ async function displayAlbums() {
         // console.log(e);
         e.addEventListener("click", async item => {
             // console.log(item.target, item.target.dataset);
-            songs = await getSongs(`Songs/${item.currentTarget.dataset.folder}/`);
+            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}/`);
             playMusic(songs[0]);
 
         });
