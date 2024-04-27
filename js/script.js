4e6bb193965fd95be2a4f4cce2cc427602df034e
Shoyab Khan <156696944+Shoyabk1707@users.noreply.github.com>
2024-04-27 14:51:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`/songs/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
